c3a7671da97a8973bafabee02b10147cebf7f8d9
Ukuma012 <Ukuma012@gmail.com>
2023-07-24 21:34:33 +0900

    modified

--- main/mother_board/rom.sv
+++ main/mother_board/rom.sv
@@ -12,8 +12,8 @@ module rom(
             4'b0100: data = 8'b1110_0011;   // 4        JNC    3
             4'b0101: data = 8'b0101_0001;   // 5        ADD B, 1
             4'b0110: data = 8'b1110_0001;   // 6        JNC    1
-            4'b0111: data = 8'b1001_0000;   // 7        OUT    0
-            4'b1000: data = 8'b1001_1111;   // 8        OUT    15
+            4'b0111: data = 8'b1011_0000;   // 7        OUT    0
+            4'b1000: data = 8'b1011_1111;   // 8        OUT    15
             4'b1001: data = 8'b1111_0111;   // 9        JMP    7
             default: data = 8'b0000_0000;
         endcase
